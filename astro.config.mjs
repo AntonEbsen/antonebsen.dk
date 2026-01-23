1102db3e70572eb6bf6e3d23817a6abf9005ccbe
AntonEbsen <anton.ebsen@gmail.com>
2026-01-23 16:54:05 +0100

    Switch to Vercel Adapter for improved deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+// import node from '@astrojs/node'; // Removed Node adapter
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  adapter: vercel({
+    webAnalytics: { enabled: true }
   }),
   site: 'https://antonebsen.dk',
 
