1ceb7114b03e4d0761843d6ca524d295aee2f586
AntonEbsen <anton.ebsen@gmail.com>
2026-01-30 20:16:52 +0100

    fix: import correct IndexPage in en/index.astro and clean build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
+  trailingSlash: 'never',
+  build: {
+    format: 'file'
+  },
   image: {
     service: passthroughImageService()
   },
